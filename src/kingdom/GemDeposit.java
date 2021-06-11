7a366bff9251a6bdc5c80abd18f7f339855198eb
Michael Bui <michaelbui99@hotmail.com>
2021-06-11 17:23:03 +0200

    Name change

--- src/kingdom/GemDeposit.java
+++ src/kingdom/GemDeposit.java
@@ -2,7 +2,7 @@ package kingdom;
 //adapter
 public class GemDeposit<Gem> implements BlockingQueueKingdom<Gem>
 {
-  private ListADT<Gem> gemList= new ArrayListKingdom<Gem>();
+  private ListADT<Gem> gemList = new ArrayListKingdom<Gem>();
   private int capacity;
 
   public GemDeposit(int capacity)
@@ -17,19 +17,19 @@ public class GemDeposit<Gem> implements BlockingQueueKingdom<Gem>
    Catalogue.getInstance().waiting("Mine worker");
    wait();
   }
-  gemListADT.add(gem);
+  gemList.add(gem);
   notifyAll();
 //  Catalogue.getInstance().gemDeposit((kingdom.gems.Gem) gem);
  }
 
  @Override public synchronized Gem dequeue() throws InterruptedException
  {
-  while (gemListADT.isEmpty())
+  while (gemList.isEmpty())
   {
    Catalogue.getInstance().waitingForGemDeposit();
    wait();
   }
-  Gem gem = gemListADT.remove(gemListADT.size()-1);
+  Gem gem = gemList.remove(gemList.size()-1);
   notifyAll();
   Catalogue.getInstance().removeFromDeposit((kingdom.gems.Gem) gem);
   return gem;
@@ -37,6 +37,6 @@ public class GemDeposit<Gem> implements BlockingQueueKingdom<Gem>
 
  @Override public boolean isFull()
  {
-  return capacity == gemListADT.size();
+  return capacity == gemList.size();
  }
 }
